d5896ce1fed9220b4471c984a471abc7d870a166
Shodiyor Ergashev <shodiyorergashev970@gmail.com>
2025-03-14 11:10:20 +0500

    argv fixed

--- argv/main.js
+++ argv/main.js
@@ -1,12 +1,16 @@
 const data = process.argv.slice(2).splice(" ");
+
 function mathFunc(arr) {
+  if (arr.length !== 3) return "Invalid input";
   const operator = arr[1];
   const a = +arr[0];
   const b = +arr[2];
+  if (isNaN(a) || isNaN(b)) return "Invalid numbers";
   if (operator === "+") return a + b;
   if (operator === "-") return a - b;
   if (operator === "/") return a / b;
   if (operator === "*") return a * b;
+  else return "Invalid operator";
 }
 
 console.log(mathFunc(data));
